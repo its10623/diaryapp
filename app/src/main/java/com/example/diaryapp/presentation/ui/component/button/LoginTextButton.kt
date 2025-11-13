1f7ef1d7d71a12a3215a3fed2d02f808cc124b83
its10623 <its10623@gmail.com>
2025-11-14 00:52:10 +0900

    feat: 로그인 TextButton Component 구현

---
+++ app/src/main/java/com/example/diaryapp/presentation/ui/component/button/LoginTextButton.kt
@@ -0,0 +1,35 @@
+package com.example.diaryapp.presentation.ui.component.button
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.unit.dp
+import com.example.diaryapp.presentation.ui.theme.PrimaryAccent
+
+@Composable
+fun TextOnlyButton(
+    text: String,
+    color: Color = PrimaryAccent,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(18.dp),
+        elevation = null,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Transparent,
+            contentColor = PrimaryAccent,
+
+        )
+    ) {
+        Text(
+            text = text
+        )
+    }
+}
