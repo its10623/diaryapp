6b26e03f8e00b420295921d459d5d712d5485c3f
its10623 <its10623@gmail.com>
2025-11-20 14:19:18 +0900

    feat: 타임라인 카드 더보기 버튼 생성

---
+++ app/src/main/java/com/example/diaryapp/presentation/ui/component/button/MoreButton.kt
@@ -0,0 +1,34 @@
+package com.example.diaryapp.presentation.ui.component.button
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.diaryapp.R
+import com.example.diaryapp.presentation.ui.theme.PrimaryAccent
+
+@Composable
+fun MoreButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        onClick = onClick,
+
+        ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_more),
+            modifier = Modifier
+                .size(35.dp),
+            contentDescription = "옵션",
+            tint = PrimaryAccent
+        )
+    }
+}
