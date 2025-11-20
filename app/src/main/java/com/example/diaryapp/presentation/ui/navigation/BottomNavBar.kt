81e5f2d48fafd5849d323a1e38c30147142e6a28
its10623 <its10623@gmail.com>
2025-11-20 14:35:03 +0900

    feat: 바텀 네비게이션 바 컴포넌트 생성

---
+++ app/src/main/java/com/example/diaryapp/presentation/ui/navigation/BottomNavBar.kt
@@ -0,0 +1,94 @@
+package com.example.diaryapp.presentation.ui.navigation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.diaryapp.presentation.ui.theme.BackGround
+import com.example.diaryapp.presentation.ui.theme.PrimaryAccent
+
+@Composable
+fun BottomNavBar(
+    currentRoute: String?,
+    onNavigate: (String) -> Unit
+) {
+    NavigationBar(
+        containerColor = PrimaryAccent
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            bottomNavScreen.forEach { screen ->
+
+                val selected = currentRoute == screen.bottomRoute
+                val interactionSource = remember { MutableInteractionSource() }
+                Box(
+                    modifier = Modifier
+                        .width(100.dp)
+                        .padding(vertical = 4.dp)
+                        .size(60.dp)
+                        .clickable(
+                            interactionSource = interactionSource,
+                            indication = null
+                        ) {
+                            onNavigate(screen.bottomRoute)
+                        }
+                        .background(
+                            color = if (selected) Color(0xFF4D4D4D)
+                            else Color.Transparent,
+                            shape = RoundedCornerShape(30.dp)
+                        )
+                        .padding(horizontal = 16.dp, vertical = 8.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column (
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            painterResource(screen.icon),
+                            null,
+                            tint = BackGround,
+                            modifier = Modifier
+                                .size(24.dp)
+                        )
+                        Spacer(Modifier.width(6.dp))
+                        Text(
+                            text = screen.title,
+                            color = BackGround,
+                            fontSize = 16.sp,
+                            maxLines = 1,
+
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
