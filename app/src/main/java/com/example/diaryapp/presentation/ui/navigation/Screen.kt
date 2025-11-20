abd7124ae4b2e837730cf7eef2942223d72aae5d
its10623 <its10623@gmail.com>
2025-11-20 14:32:10 +0900

    feat: 네비게이션 경로 저장하는 Sead Class 정의

---
+++ app/src/main/java/com/example/diaryapp/presentation/ui/navigation/Screen.kt
@@ -0,0 +1,29 @@
+package com.example.diaryapp.presentation.ui.navigation
+
+import com.example.diaryapp.R
+
+sealed class Screen(
+    val route: String
+) {
+    sealed class Bottom(val bottomRoute: String, val title: String, val icon: Int) : Screen(bottomRoute) {
+        object Timeline : Bottom("timeline","타임라인",R.drawable.ic_timeline)
+        object Calender : Bottom("calender", "달력", R.drawable.ic_calendar_month_24dp_e3e3e3_fill0_wght400_grad0_opsz24)
+        object Profile : Bottom("profile","프로필",R.drawable.ic_account_circle_24dp_e3e3e3_fill0_wght400_grad0_opsz24)
+    }
+
+    object Main : Screen("main")
+    object Login : Screen("login")
+    object FindScreen : Screen("findScreen")
+    object SignUp : Screen("signup")
+    object FolderScreen : Screen("folderScreen")
+    object WriteScreen : Screen("writeScreen")
+    object EditScreen : Screen("edit/{id}")
+    object ViewDiary : Screen("viewDiary")
+
+}
+
+val bottomNavScreen = listOf(
+    Screen.Bottom.Timeline,
+    Screen.Bottom.Calender,
+    Screen.Bottom.Profile
+)
