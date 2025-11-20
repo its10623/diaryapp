9804f51e45bd4a0eabf53ca630734b461f39bcd7
its10623 <its10623@gmail.com>
2025-11-21 01:05:45 +0900

    refactor: 불필요한 인터페이스 및 구현체 삭제

--- app/src/main/java/com/example/diaryapp/application/usecase/UserUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.diaryapp.application.usecase
-
-import com.example.diaryapp.dto.UserDto
-
-interface UserUseCase {
-    fun signUpUseCase(userDto: UserDto)
-    fun loginUseCase(userDto: UserDto): UserDto
-}
--- app/src/main/java/com/example/diaryapp/presentation/viewmodel/UserViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.diaryapp.presentation.viewmodel
-
-import com.example.diaryapp.dto.UserDto
-
-interface UserViewModel {
-    fun onSignUp(userDto: UserDto): Boolean
-    fun onLogin(userDto: UserDto): UserDto?
-}
