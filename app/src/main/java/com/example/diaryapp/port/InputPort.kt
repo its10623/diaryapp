ce2b2a6f0696afab455d7ea781c6e7503940624e
its10623 <its10623@gmail.com>
2025-11-24 23:13:06 +0900

    refactor: 불 필요한 클래스 삭제

--- app/src/main/java/com/example/diaryapp/port/InputPort.kt
+++
@@ -1,8 +0,0 @@
-package com.example.diaryapp.port
-
-interface InputPort {
-    fun readUserName(): String
-    fun readPassword(): String
-    fun readTitle(): String
-    fun readContent(): String
-}
--- app/src/main/java/com/example/diaryapp/presentation/ui/component/EditorMode.kt
+++
@@ -1,5 +0,0 @@
-package com.example.diaryapp.presentation.ui.component
-
-enum class EditorMode {
-    CREATE,EDIT
-}
